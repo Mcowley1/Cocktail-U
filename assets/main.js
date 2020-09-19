1149e7e8a9ba531ca250a8b1b72594a96001fa66
Damaris C <DCampos07@users.noreply.github.com>
2020-09-18 20:02:49 -0400

    selectable code added to drinks

--- assets/main.js
+++ assets/main.js
@@ -2,7 +2,8 @@
 var searchButton = $(".searchButton");
 var drinksListEl = $("#drink-list");
 var drinksHistory = $("#saved-searches");
-
+var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
+var drinks = "" || searchHistory[0];
 
 
 // for the nav bar
@@ -17,9 +18,18 @@ $(function() {
     });
   });
   
-  
-  var searchHistory = JSON.parse(localStorage.getItem("searchHistory")) || [];
-  var drinks = "" || searchHistory[0];
+  // Forloop for persisting the drinks on the DOM
+  function loadHistory(){
+      for (var i = 0; i < searchHistory.length; i++) {
+          var historyDivs = $('<div>');
+          historyDivs.addClass("saved-items");
+          historyDivs.addClass('list-group-item');
+          historyDivs.innerHTML(searchHistory[i]);
+          historyDivs.attr("data-index", searchHistory[i]);
+          drinkHistory.append(historyDivs);
+      }
+  } 
+
 // Get Drinks
 // Forloop for persisting the data on the DOM
 function loadHistory(){
@@ -44,7 +54,34 @@ function loadHistory(){
       console.log(response)
     })
 });
-
+// Search button click event
+searchButton.click(function (event) {
+  // event.preventDefault();
+  var searchInput = $(".searchInput").val();
+  if (searchInput !== "") {
+      // var list = $("#saved-searches");
+      var drinksName = $("<li>");
+      var drinksName2 = $("<a>");
+      drinksName2.attr("href","#");
+      drinksName2.text(searchInput);
+      drinksName.attr("data-index", searchInput);
+      drinksName.addClass("saved-items");
+      // cityName.text(searchInput);
+      // // Local storage
+      localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+      drinksName.append(drinksName2);
+      drinksHistory.append(drinksName);
+  }
+  getDrinks(searchInput);
+});
+$(document).on("click", ".saved-items", function(){
+  var drinks = $(this).attr("data-index");
+  console.log(city)
+  getDrinks(city);
+});
+$(document).ready(function() {
+  loadHistory();
+});
 
 
 // take id from results and add data index in case its clicked.
