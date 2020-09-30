4315cda8134e2873ee8254c9257d2e98d262a2dc
Cowleyfitcode <65091695+Mcowley1@users.noreply.github.com>
2020-09-29 22:49:51 -0400

    Update landing.js

--- assets/landing.js
+++ assets/landing.js
@@ -2,8 +2,22 @@
 var nameInsert = document.getElementById("name");
 var emailInsert = document.getElementById("email");
 
+var access_key = "14742816210f0dbd6b2a2953b0230963";
+var email_address = "support@apilayer.net";
+// verify email address via AJAX call
+$.ajax({
+    method: 'GET',
+    url: 'http://apilayer.net/api/check?access_key=' + '14742816210f0dbd6b2a2953b0230963' + '&email=' + email_address,
+    success: function(json) {
+    
+    // Access and use your preferred validation result objects
+    console.log(json.format_valid);
+    console.log(json.smtp_check);
+    console.log(json.score);
+    }
+});
 
-
+// moves page to main page and set local storage
 document.getElementById('myform').addEventListener('submit', function(e) {
   e.preventDefault()
   var key = nameInsert.value;
@@ -12,36 +26,9 @@ document.getElementById('myform').addEventListener('submit', function(e) {
   console.log(key);
   console.log(value);
 
-  
 
   if (key && value) {
     localStorage.setItem(key, value);
-    window.location.href = "main.html"
+    window.location.href = "./main.html"
   }
-  verifyEmail(value);
 });
-// set endpoint and your access key
-
-// verify email address via AJAX call
-function verifyEmail(email) {
-
-    var emailV = $(".email-V");
-    var access_key = "14742816210f0dbd6b2a2953b0230963";
-    var email_address = "support@apilayer.net";
-    
-    $.ajax({
-      url:
-        "http://apilayer.net/api/check?access_key=" +
-        access_key +
-        "&email=" +
-        email,
-      dataType: "json",
-      success: function (json) {
-        // Access and use your preferred validation result objects
-        console.log(json.format_valid);
-        console.log(json.smtp_check);
-        console.log(json.score);
-      },
-    });
-}
-
